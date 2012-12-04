5ca28048a212c4f94738f491963db8495dc835dd
marceleandro <marceleap0425@yahoo.com>
2012-12-03 21:38:00 -0600

    Nueva presentacion de Motivo de Consulta

--- app/models/motivo_consulta.rb
+++ app/models/motivo_consulta.rb
@@ -1,13 +1,12 @@
 class MotivoConsulta < ActiveRecord::Base
   belongs_to :expediente
   
+  
   ESTADO_ACTIVO = :activo
   ESTADO_INACTIVO = :inactivo
   ESTADO_SUSPENDIDO = :suspendido
   ESTADO_ABANDONADO = :abandonado
   ESTADO_CERRADO = :cerrado
   
-  def inactivo?
-    estado.to_s == ESTADO_INACTIVO.to_s
-  end
+  
 end
